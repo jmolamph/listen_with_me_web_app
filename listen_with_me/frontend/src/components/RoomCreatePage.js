e074b3cadbfbc909f319db2d535eb83d273a22de
jmolamph <jmolamph@uci.edu>
2022-09-29 15:34:56 -0700

    Set up routing from home page
    Set up home page router

---
+++ listen_with_me/frontend/src/components/RoomCreatePage.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+
+export default class RoomCreatePage extends Component {
+    constructor(props ) {
+        super(props);
+
+    }
+
+    render() {
+        return <p>This is the create room page</p>;
+    }
+}
--- listen_with_me/frontend/src/components/app.js
+++ listen_with_me/frontend/src/components/app.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
+    // renders the html on the page
     render() {
-        return (<h1>Testing React Code</h1>);
+        return ( <div>
+            <HomePage />
+        </div>
+        );
     }
 }
 
+// render App component in the div
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
